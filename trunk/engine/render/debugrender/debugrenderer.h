fda60658a2cf017d9f118018634ffafe8358ff75
fredrik <fredrik@5dc30038-7a27-d146-a8f3-f54a8de86565>
2017-01-04 08:00:08 +0000

    Added some files

---
+++ trunk/engine/render/debugrender/debugrenderer.h
@@ -0,0 +1,26 @@
+#pragma once
+
+//make sure to include all shapes so that we can just include this file if no matter what we want to use
+#include "shape.h"
+#include "line.h"
+
+
+namespace Debug
+{
+
+class DebugRenderer
+{
+public:
+	DebugRenderer();
+	~DebugRenderer();
+
+
+
+private:
+
+
+
+};
+
+
+}
--- trunk/resources/shaders/fragment/line_colored.frag
+++ trunk/resources/shaders/fragment/line_colored.frag
@@ -2,5 +2,5 @@ out vec4 diffuseColor;
 
 void main()
 {
-	diffuseColor = vec4(0.5, 0.5, 0.0, 0.5);
+	diffuseColor = vec4(1.0, 1.0, 0.0, 1.0);
 }
