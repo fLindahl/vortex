d3270cd0beb6b1a9b1c4c366a706b3f03061be6d
fredrik <fredrik@5dc30038-7a27-d146-a8f3-f54a8de86565>
2017-01-05 08:03:06 +0000

    Added box debug rendering

--- engine/render/debugrender/debugrenderer.cpp
+++ engine/render/debugrender/debugrenderer.cpp
@@ -11,7 +11,9 @@ DebugRenderer::DebugRenderer()
 
 void DebugRenderer::Initialize()
 {
-    this->line.shaderProgram = Render::ShaderServer::Instance()->LoadShader("debugLines")->GetProgram();
+    this->line.shader = Render::ShaderServer::Instance()->LoadShader("debugLines");
+	this->box.shader = Render::ShaderServer::Instance()->LoadShader("debug");
+
     return;
 }
 
@@ -32,6 +34,98 @@ void DebugRenderer::DrawLine(const Math::point &startPoint,
     this->commandQueue.push(cmd);
 }
 
+
+void DebugRenderer::DrawBox(const Math::vector& position,
+							const Math::quaternion& rotation, 
+							const float& scale, 
+							const Math::vec4& color, 
+							const bool& wireframe, 
+							const float& lineWidth)
+{
+	BoxCommand* cmd = new BoxCommand();
+	cmd->shape = DebugShape::BOX;
+
+	Math::mat4 transform = Math::mat4::multiply(Math::mat4::scaling(scale), Math::mat4::rotationquaternion(rotation));
+	transform.translate(position);
+
+	cmd->transform = transform;
+		
+	cmd->lineWidth = lineWidth;
+	cmd->color = color;
+	cmd->wireframe = wireframe;
+
+	this->commandQueue.push(cmd);
+
+}
+
+void DebugRenderer::DrawBox(const Math::vector& position, 
+							const Math::quaternion& rotation, 
+							const float& width, 
+							const float& height, 
+							const float& length, 
+							const Math::vec4& color, 
+							const bool& wireframe, 
+							const float& lineWidth)
+{
+	BoxCommand* cmd = new BoxCommand();
+	cmd->shape = DebugShape::BOX;
+
+	Math::mat4 transform = Math::mat4::multiply(Math::mat4::scaling(Math::point(width, height, length)), Math::mat4::rotationquaternion(rotation));
+	transform.translate(position);
+
+	cmd->transform = transform;
+
+	cmd->lineWidth = lineWidth;
+	cmd->color = color;
+	cmd->wireframe = wireframe;
+
+	this->commandQueue.push(cmd);
+}
+
+void DebugRenderer::DrawBox(const Math::mat4& transform, 
+							const Math::vec4& color, 
+							const bool& wireframe, 
+							const float& lineWidth)
+{
+	BoxCommand* cmd = new BoxCommand();
+	cmd->shape = DebugShape::BOX;
+
+	cmd->transform = transform;
+
+	cmd->lineWidth = lineWidth;
+	cmd->color = color;
+	cmd->wireframe = wireframe;
+
+	this->commandQueue.push(cmd);
+}
+
+void DebugRenderer::DrawBox(const Math::bbox& bbox, 
+							const Math::vec4& color, 
+							const float& lineWidth)
+{
+	BoxCommand* cmd = new BoxCommand();
+	cmd->shape = DebugShape::BOX;
+
+	float width = bbox.get_maxx() - bbox.get_minx();
+	float height = bbox.get_maxy() - bbox.get_miny();
+	float length = bbox.get_maxz() - bbox.get_minz();
+	
+	Math::vector position = (bbox.get_minpoint() + bbox.get_maxpoint()) * 0.5f;
+
+	Math::mat4 transform = Math::mat4::scaling(Math::point(width, height, length));
+	transform.translate(position);
+
+	cmd->transform = transform;
+
+	cmd->lineWidth = lineWidth;
+	cmd->color = color;
+	//Always draw bboxes with wireframe
+	cmd->wireframe = true;
+
+	this->commandQueue.push(cmd);
+}
+
+
 void DebugRenderer::DrawCommands()
 {
     while (!this->commandQueue.empty())
@@ -43,11 +137,14 @@ void DebugRenderer::DrawCommands()
         {
             case DebugShape::LINE:
             {
-                glUseProgram(this->line.shaderProgram);
-                glPolygonMode(GL_FRONT, GL_LINE);
                 this->line.Draw(currentCommand);
                 break;
             }
+			case DebugShape::BOX:
+			{
+				this->box.Draw(currentCommand);
+				break;
+			}
             default:
             {
                 printf("Debug::RenderShape not fully implemented!\n");
@@ -59,8 +156,6 @@ void DebugRenderer::DrawCommands()
 
         delete currentCommand;
     }
-
-    glPolygonMode(GL_FRONT, GL_FILL);
 
     return;
 }
