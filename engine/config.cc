9d94b324f97b205572a31a754ac496a0e95778b1
VisualSVN Server <VisualSVN Server@5dc30038-7a27-d146-a8f3-f54a8de86565>
2017-01-05 20:41:28 +0000

    Deleted folder '/engine'.

--- engine/config.cc
+++
@@ -1,9 +0,0 @@
-//------------------------------------------------------------------------------
-/**
-    @file core/config.cc
-    
-	Main configure file for types and OS-specific stuff.
-	
-	(C) 2015 See the LICENSE file.
-*/
-#include "config.h"
