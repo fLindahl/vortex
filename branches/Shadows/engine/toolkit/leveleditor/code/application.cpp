648b926fbae9488ac721dd786909bd1c2f5d2489
gregor <gregor@5dc30038-7a27-d146-a8f3-f54a8de86565>
2017-02-07 15:00:12 +0000

    #magicupdate

--- branches/Shadows/engine/toolkit/leveleditor/code/application.cpp
+++ branches/Shadows/engine/toolkit/leveleditor/code/application.cpp
@@ -85,7 +85,35 @@ Application::Open()
 		sTransform.translate(Math::vector(0.0f, -2.0f, 0.0f));
 		this->sponza->SetTransform(sTransform);
 
-		//this->sponza->SetTransform(sTransform);
+		/*
+		this->wall1 = std::make_shared<Game::ModelEntity>();
+		this->wall2 = std::make_shared<Game::ModelEntity>();
+		this->wall3 = std::make_shared<Game::ModelEntity>();
+		this->wall4 = std::make_shared<Game::ModelEntity>();
+		this->floor = std::make_shared<Game::ModelEntity>();
+		this->ceiling = std::make_shared<Game::ModelEntity>();
+
+		this->wall1->SetModel(ResourceServer::Instance()->LoadModel("resources/models/groundfloor.mdl"));
+		this->wall2->SetModel(ResourceServer::Instance()->LoadModel("resources/models/groundfloor.mdl"));
+		this->wall3->SetModel(ResourceServer::Instance()->LoadModel("resources/models/groundfloor.mdl"));
+		this->wall4->SetModel(ResourceServer::Instance()->LoadModel("resources/models/groundfloor.mdl"));
+		this->floor->SetModel(ResourceServer::Instance()->LoadModel("resources/models/groundfloor.mdl"));
+		this->ceiling->SetModel(ResourceServer::Instance()->LoadModel("resources/models/groundfloor.mdl"));
+
+		this->wall1->Activate();
+		this->wall2->Activate();
+		this->wall3->Activate();
+		this->wall4->Activate();
+		this->floor->Activate();
+		this->ceiling->Activate();
+
+		this->floor->SetTransform(Math::mat4::translation(0.0f, -2.0f, 0.0f));
+		this->wall2->SetTransform(Math::mat4::multiply(Math::mat4::rotationz(1.57f), Math::mat4::translation(10.0f, 8.0f, 0.0f)));
+		this->wall3->SetTransform(Math::mat4::multiply(Math::mat4::rotationz(1.57f), Math::mat4::translation(-10.0f, 8.0f, 0.0f)));
+		this->wall4->SetTransform(Math::mat4::multiply(Math::mat4::rotationx(1.57f), Math::mat4::translation(0.0f, 8.0f, 10.0f)));
+		this->wall1->SetTransform(Math::mat4::multiply(Math::mat4::rotationx(1.57f), Math::mat4::translation(0.0f, 8.0f, -10.0f)));
+		this->ceiling->SetTransform(Math::mat4::translation(0.0f, 18.0f, 0.0f));
+		*/
 
 
 /*		PointLight pLight;
@@ -114,7 +142,7 @@ Application::Open()
         sLight.angle = 15.0f;
 		LightServer::Instance()->AddSpotLight(sLight);
 
-	    sLight.position = Math::vec4(-6.0f, 3.5f, 0.0f, 1.0f);
+/*	    sLight.position = Math::vec4(4.0f, 3.5f, 0.0f, 1.0f);
 		sLight.color = Math::vec4(0.0f, 1.0f, 0.0f, 1.0f);
 		sLight.coneDirection = Math::vec4(1.0f, 0.0f, 0.0f, 1.0f);
 		sLight.length = 15.0f;
@@ -130,9 +158,9 @@ Application::Open()
 		sLight.position = Math::vec4(-2.1f, 15.0f, -6.0f, 1.0f);
 		sLight.color = Math::vec4(1.0f, 1.0f, 0.0f, 1.0f);
 		sLight.coneDirection = Math::vec4(0.0f, -1.0f, 0.0f, 1.0f);
-		sLight.length = 10.0f;
+		sLight.length = 18.0f;
 		sLight.angle = 5.0f;
-		LightServer::Instance()->AddSpotLight(sLight);
+		LightServer::Instance()->AddSpotLight(sLight);*/
 
 		// set ui rendering function
 		this->window->SetUiRender([this]()
