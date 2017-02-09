d0d2bf89d5c50b9336e4f131f43389b7a63479c1
Fredrik Lindahl <Dunderbullen@users.noreply.github.com>
2017-02-09 12:28:01 +0000

    cont. cubemaps

--- engine/render/resources/cubemapnode.h
+++ engine/render/resources/cubemapnode.h
@@ -11,6 +11,12 @@ namespace Render
 class CubeMapNode : public std::enable_shared_from_this<CubeMapNode>
 {
 public:
+	enum CubemapShape
+	{
+		BOX,
+		SPHERE
+	};
+
 	CubeMapNode();
 	~CubeMapNode();
 
@@ -26,6 +32,12 @@ public:
 	void DeleteCubeMap();
 
 	bool IsLoaded();
+
+	Math::vector& InnerScale() { return this->innerScale; }
+	Math::vector& OuterScale() { return this->outerScale; }
+
+	void SetShape(CubemapShape s) { this->shape = s; }
+	CubemapShape GetShape() const { return this->shape; }
 
 private:
 	enum CubeFace
@@ -45,13 +57,23 @@ private:
 
 	Math::point position;
 
+	// cubemaps influence shape
+	// box will use entire scale vectors, sphere will always be spherical (x as radius)
+	CubemapShape shape;
+	// if camera is within innerbounds, 100% of this cubemap will be used.
+	Math::vector innerScale;
+	// cubemaps will be blended depending on the cameras position relative to inner and outer range for k cubemaps.
+	Math::vector outerScale;
+
 	Render::Resolution resolution;
 	GLuint mipLevels;
 
 	///The cubemap
 	GLuint cubeSampler;
 
-	//Lists all textures in the cubemap
+#ifdef _LEVELEDITOR
+	//Lists all textures in the cubemap so that we can inspect them within applications
 	GLuint textures[6];
+#endif
 };
 }
